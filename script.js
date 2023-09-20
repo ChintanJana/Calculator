f20a8954c1670650940ae1ddf9e4e4ffd44308fe
ChintanJana <117261030+ChintanJana@users.noreply.github.com>
2023-09-20 21:52:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-// Todo: Make M+ M- and MC functional
-let string = "";
-let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
-  button.addEventListener('click', (e)=>{
-    if(e.target.innerHTML == '='){
-      string = eval(string);
-      document.querySelector('input').value = string;
-    }
-    else if(e.target.innerHTML == 'C'){
-      string = ""
-      document.querySelector('input').value = string;
-    }
-    else{ 
-    console.log(e.target)
-    string = string + e.target.innerHTML;
-    document.querySelector('input').value = string;
+document.addEventListener('DOMContentLoaded', function () {
+  let string = '';
+  let memory = 0;
+  let inputField = document.querySelector('.input');
+  let buttons = document.querySelectorAll('.button');
+  
+  buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      if (e.target.innerHTML === '=') {
+        try {
+          string = eval(string);
+          inputField.value = string;
+        } catch (error) {
+          inputField.value = 'Error';
+        }
+      } else if (e.target.innerHTML === 'C') {
+        string = '';
+        inputField.value = string;
+      } else if (e.target.innerHTML === 'M+') {
+        memory += parseFloat(string) || 0;
+        inputField.value = string;
+      } else if (e.target.innerHTML === 'M-') {
+        memory -= parseFloat(string) || 0;
+        inputField.value = string;
+      } else if (e.target.innerHTML === 'MR') {
+        inputField.value = memory.toString(); // Display memory value
+      } else {
+        string = string + e.target.innerHTML;
+        inputField.value = string;
       }
-  })
-})
+    });
+  });
+});
